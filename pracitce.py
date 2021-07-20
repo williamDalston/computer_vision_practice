00127b3fcadda3c84295e1a813b2d5b6618c3fc4
William Alston <faradaybach@gmail.com>
2021-07-20 13:54:23 -0400

    import from sklearn

--- pracitce.py
+++ pracitce.py
@@ -6,3 +6,9 @@ import numpy as np
 arr_float_3d = np.ones((3,2,4))
 print(arr_float_3d)
 
+#download MNIST dataset of handwritten digits
+
+from sklearn import datasets
+mnist = datasets.fetch_mldata('MNIST origional')
+mnist.data.shape  #contains 70,000 images of 28x28 =784 pixels each
+mnist.target.shape

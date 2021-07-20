e5a8abee74d80474e76dcd9d4d85d53c79e550b4
William Alston <faradaybach@gmail.com>
2021-07-20 13:43:15 -0400

    cv2 notes

--- pracitce.py
+++ pracitce.py
@@ -1,2 +1,8 @@
-#As I learn I will write the code below.
+
+import numpy as np
+
+#create a small image that reflects the three color channels
+
+arr_float_3d = np.ones((3,2,4))
+print(arr_float_3d)
 
